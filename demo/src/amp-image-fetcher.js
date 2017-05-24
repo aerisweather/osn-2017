408e0dacb76b491c5eeb44cd72faf084352a07e6
Edan Schwartz <edanschwartz@gmail.com>
2017-05-24 15:31:04 -0500

    amp-image-fetcher is working :)
    - fix encoding
    - do not try to stream request
      (`request` module does not actually return a
       real ReadStream)
    - document

--- demo/src/amp-image-fetcher.js
+++ demo/src/amp-image-fetcher.js
@@ -1,27 +1,41 @@
 const moment = require('moment');
-const request = require('request');
+const request = require('request-promise');
 const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3();
 const lambda = new AWS.Lambda();
 
+/**
+ * Payload:
+ * {
+ * 	 imageId: string;
+ * 	 layers: string[];
+ * 	 width: number;
+ * 	 height: number;
+ * 	 center: string;
+ * 	 zoom: number;
+ *	 validTime: Date; // ISO format
+ * }
+ */
 exports.handler = async (event, context, callback) => {
 	try {
-		console.log(`Received event: ${JSON.stringify(event)}`);
+		console.log(`Received event: ${JSON.stringify(event, null, 2)}`);
 		// Figure out AMP endpoint, from event payload
 		const endpoint = [
 			`/${process.env.CLIENT_ID}_${process.env.CLIENT_SECRET}`,
 			`/${event.payload.layers.join(',')}`,
 			`/${event.payload.width}x${event.payload.height}`,
 			`/${event.payload.center},${event.payload.zoom}`,
-			`/${moment(event.payload.validTime).format('YYMMDDHHmmss')}`,
+			`/${moment(event.payload.validTime).format('YYYYMMDDHHmmss')}`,
 			`.png`
 		].join('');
 
-		// Read the image from AMP
-		const readStream = request(`http://maps.aerisapi.com/${endpoint}`);
+		// Download the image from AMP
+		const image = await request(`http://maps.aerisapi.com/${endpoint}`, {
+			encoding: null
+		});
 
-		// And write the image back up to S3
+		// And upload the image back up to S3
 		const s3Location = {
 			bucket: 'aeris-osn-2017',
 			key: `amp-image-fetcher${endpoint}`
@@ -29,7 +43,7 @@ exports.handler = async (event, context, callback) => {
 		await s3.upload({
 			Bucket: s3Location.bucket,
 			Key: s3Location.key,
-			Body: readStream
+			Body: image
 		}).promise();
 
 		// Send a message to the mediator,
@@ -45,8 +59,9 @@ exports.handler = async (event, context, callback) => {
 		await lambda.invoke({
 			FunctionName: 'mediator',
 			InvocationType: 'Event',
-			Payload: outMessages
+			Payload: JSON.stringify(outMessages)
 		}).promise();
+		console.log(`Completed with: ${JSON.stringify(outMessages, null, 2)}`);
 
 		callback();
 	}
