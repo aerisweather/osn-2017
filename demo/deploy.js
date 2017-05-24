afb2f50c9efd545e1f0e81e84f915d10ca38816d
Edan Schwartz <edanschwartz@gmail.com>
2017-05-24 14:57:12 -0500

    Add deploy script

---
+++ demo/deploy.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+const AWS = require('aws-sdk');
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+(() => {
+  AWS.config.loadFromPath(`${__dirname}/deploy-credentials.ignore.json`);
+
+  // Create a zip archive of the code
+  console.log('Archiving code...');
+  const archiveFile = `${__dirname}/archive.zip`;
+  execSync(`zip -r -9 ${archiveFile} ./`, {
+    cwd: process.cwd()
+  });
+  // Load the archive into memory, and delete the file
+  const archive = fs.readFileSync(archiveFile);
+  fs.unlinkSync(archiveFile);
+  console.log('Archiving code... done.');
+
+  // Update code for all lambda functions
+  console.log(`Uploading code to lambda functions...`);
+  const lambda = new AWS.Lambda({ region: 'us-east-1' });
+  const lambdaFunctions = [
+
+    'amp-image-fetcher',
+    //'gif-creator',
+    //'thumbnail'
+  ];
+  return Promise.all(
+    lambdaFunctions.map(fName =>
+      lambda.updateFunctionCode({
+        FunctionName: fName,
+        Publish: true,
+        ZipFile: archive
+      }).promise()
+        .then(res => console.log(`Uploaded ${fName} v${res.Version}`))
+    )
+  )
+  .then(() => {
+    console.log(`Uploading code to lambda functions... done!`)
+  })
+})()
+  .then(
+    () => {
+      console.log('Deployment complete');
+      process.exit(0);
+    },
+    err => {
+      console.error(`Deployment failed: ${err}`);
+      process.exit(1);
+    }
+  );
