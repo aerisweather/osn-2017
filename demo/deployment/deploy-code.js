d337593cb8d9206f3e66283db021ad16e08bdf56
Edan Schwartz <edanschwartz@gmail.com>
2017-05-26 12:23:51 -0500

    Move deploy-code.js

--- demo/deployment/deploy-code.js
+++ demo/deployment/deploy-code.js
@@ -7,7 +7,7 @@ const fs = require('fs');
   AWS.config.loadFromPath(`${__dirname}/deploy-credentials.ignore.json`);
 
   // Create a zip archive of the code
-  console.log('Archiving code...');
+	console.log('Archiving code...');
   const archiveFile = `${__dirname}/archive.zip`;
   execSync(`zip -r -9 ${archiveFile} ./`, {
     cwd: process.cwd()
@@ -15,15 +15,16 @@ const fs = require('fs');
   // Load the archive into memory, and delete the file
   const archive = fs.readFileSync(archiveFile);
   fs.unlinkSync(archiveFile);
-  console.log('Archiving code... done.');
+	console.log('Archiving code... done.');
 
   // Update code for all lambda functions
-  console.log(`Uploading code to lambda functions...`);
+	console.log(`Uploading code to lambda functions...`);
   const lambda = new AWS.Lambda({ region: 'us-east-1' });
   const lambdaFunctions = [
-    'amp-image-fetcher',
-    'gif-creator',
-    'thumbnail-creator'
+    'osn2017-amp-image-fetcher',
+    'osn2017-gif-creator',
+    'osn2017-thumbnail-creator',
+    'osn2017-mediator'
   ];
   return Promise.all(
     lambdaFunctions.map(fName =>
