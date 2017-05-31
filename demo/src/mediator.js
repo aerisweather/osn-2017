ce204dafc5da716df2534f7cc0cb374e57bdcda7
Seth Miller <smiller@aerisweather.com>
2017-05-31 11:25:45 -0500

    Added redisDataFlow connection to mediator

--- demo/src/mediator.js
+++ demo/src/mediator.js
@@ -1,11 +1,17 @@
 const RedisDataFlow = require('./db/RedisDataFlow');
 
+const redisDataFlow = new RedisDataFlow({
+	host: process.env.REDIS_HOSTNAME,
+	port: process.env.REDIS_PORT
+});
+
 exports.handler = async (event, context, callback) => {
 	ctx.callbackWaitsForEmptyEventLoop = false;
 	try {
 		console.log(`Received event: ${JSON.stringify(event, null, 2)}`);
 
 		// Save event
+
 	}
 	catch(err) {
 		console.error(err);
