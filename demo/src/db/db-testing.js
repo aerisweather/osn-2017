9caadd01428cd317a1bc2414426ea4257493179e
Edan Schwartz <edanschwartz@gmail.com>
2017-05-31 11:23:53 -0500

    Refactor db-testing
    it's kind of my thing

--- demo/src/db/db-testing.js
+++ demo/src/db/db-testing.js
@@ -1,90 +1,101 @@
 const RedisDataFlow = require("./RedisDataFlow");
 
-const messages = [
-	{
-		type:        'please-create-thumbnail',
-		dateCreated: new Date(1000).getTime(),
-		target:      'thumbnail-creator',
-		// Payload
-		imageId:     'radar-mn',
-		validTime:   new Date(600).getTime(),
-		width:       100,
-		height:      100,
-		location:    's3://aeris-osn2017/image-ingestor/radar-mn/6'
-	},
-	{
-		type:        'please-create-thumbnail',
-		dateCreated: new Date(1005).getTime(),
-		target:      'thumbnail-creator',
-		// Payload
-		imageId:     'satellite-ca',
-		validTime:   new Date(600).getTime(),
-		width:       100,
-		height:      100,
-		location:    's3://aeris-osn2017/image-ingestor/satellite-ca/6'
-	},
-	{
-		type:        'did-create-thumbnail',
-		dateCreated: new Date(1500).getTime(),
-		target:      'thumbnail-creator',
-		// Payload
-		imageId:     'radar-mn',
-		validTime:   new Date(600).getTime(),
-		width:       100,
-		height:      100,
-		location:    's3://aeris-osn2017/thumbnail-creator/radar-mn/6'
-	},
-	{
-		type:        'did-create-thumbnail',
-		dateCreated: new Date(1505).getTime(),
-		target:      'thumbnail-creator',
-		// Payload
-		imageId:     'satellite-ca',
-		validTime:   new Date(600).getTime(),
-		width:       100,
-		height:      100,
-		location:    's3://aeris-osn2017/thumbnail-creator/satellite-ca/6'
-	},
-	{
-		type:        'did-create-thumbnail',
-		dateCreated: new Date(2500).getTime(),
-		target:      'thumbnail-creator',
-		// Payload
-		imageId:     'radar-mn',
-		validTime:   new Date(700).getTime(),
-		width:       100,
-		height:      100,
-		location:    's3://aeris-osn2017/thumbnail-creator/radar-mn/7'
-	},
-	{
-		type:        'did-create-thumbnail',
-		dateCreated: new Date(3500).getTime(),
-		target:      'thumbnail-creator',
-		// Payload
-		imageId:     'radar-mn',
-		validTime:   new Date(800).getTime(),
-		width:       100,
-		height:      100,
-		location:    's3://aeris-osn2017/thumbnail-creator/radar-mn/8'
-	}
-];
 
-const client = new RedisDataFlow();
+(async () => {
+	const client = new RedisDataFlow();
 
-messages.reduce((savePromise, message) => {
-	return savePromise.then(() => client.save(message));
-}, Promise.resolve())
-	.then(() => {
-		return client.findSince({
-			type:    'did-create-thumbnail',
-			imageId: 'radar-mn'
-		}, 600)
-	})
-	.then(result => {
-		console.log(JSON.stringify(result, null, 2));
-		process.exit(0);
-	})
-	.catch(console.error);
+	// Create a bunch of messages
+	const messages = [
+		{
+			type:        'please-create-thumbnail',
+			dateCreated: new Date(1000).getTime(),
+			target:      'thumbnail-creator',
+			// Payload
+			imageId:     'radar-mn',
+			validTime:   new Date(600).getTime(),
+			width:       100,
+			height:      100,
+			location:    's3://aeris-osn2017/image-ingestor/radar-mn/6'
+		},
+		{
+			type:        'please-create-thumbnail',
+			dateCreated: new Date(1005).getTime(),
+			target:      'thumbnail-creator',
+			// Payload
+			imageId:     'satellite-ca',
+			validTime:   new Date(600).getTime(),
+			width:       100,
+			height:      100,
+			location:    's3://aeris-osn2017/image-ingestor/satellite-ca/6'
+		},
+		{
+			type:        'did-create-thumbnail',
+			dateCreated: new Date(1500).getTime(),
+			target:      'thumbnail-creator',
+			// Payload
+			imageId:     'radar-mn',
+			validTime:   new Date(600).getTime(),
+			width:       100,
+			height:      100,
+			location:    's3://aeris-osn2017/thumbnail-creator/radar-mn/6'
+		},
+		{
+			type:        'did-create-thumbnail',
+			dateCreated: new Date(1505).getTime(),
+			target:      'thumbnail-creator',
+			// Payload
+			imageId:     'satellite-ca',
+			validTime:   new Date(600).getTime(),
+			width:       100,
+			height:      100,
+			location:    's3://aeris-osn2017/thumbnail-creator/satellite-ca/6'
+		},
+		{
+			type:        'did-create-thumbnail',
+			dateCreated: new Date(2500).getTime(),
+			target:      'thumbnail-creator',
+			// Payload
+			imageId:     'radar-mn',
+			validTime:   new Date(700).getTime(),
+			width:       100,
+			height:      100,
+			location:    's3://aeris-osn2017/thumbnail-creator/radar-mn/7'
+		},
+		{
+			type:        'did-create-thumbnail',
+			dateCreated: new Date(3500).getTime(),
+			target:      'thumbnail-creator',
+			// Payload
+			imageId:     'radar-mn',
+			validTime:   new Date(800).getTime(),
+			width:       100,
+			height:      100,
+			location:    's3://aeris-osn2017/thumbnail-creator/radar-mn/8'
+		}
+	];
+
+
+	// Save the messages to the db
+	await messages.map(msg => client.save(msg));
+
+	// Look for a message
+	const results = await client.findLatest({
+		type:    'did-create-thumbnail',
+		imageId: 'radar-mn'
+	}, 600);
+
+	// Log the msg
+	console.log(JSON.stringify(results, null, 2));
+})()
+	.then(
+		() => {
+			process.exit(0);
+		},
+		err => {
+			console.error(err.stack);
+			process.exit(1);
+		}
+	);
 
 /*
  const primaryKey = message.type + message.dateCreated.getTime();
