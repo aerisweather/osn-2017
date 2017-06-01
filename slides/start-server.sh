ccffb9ac2dadb979fba176d29ad44217af38513b
Seth Miller <smiller@aerisweather.com>
2017-06-01 17:26:10 -0500

    Fixed start server script

--- slides/start-server.sh
+++ slides/start-server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd ${DIR};
+echo "Starting presentation web server";
+echo "http://localhost:8000/slideshow.html";
 python3 -m http.server
